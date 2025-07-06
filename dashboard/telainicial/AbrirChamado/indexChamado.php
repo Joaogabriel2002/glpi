46e90f7d0e57c145852203d81fe969e476fc7d34
João Gabriel <joaoogbriel3meia@gmail.com>
2025-07-06 20:41:39 -0300

    Ajustes do aside

--- dashboard/telainicial/AbrirChamado/indexChamado.php
+++ dashboard/telainicial/AbrirChamado/indexChamado.php
@@ -2,7 +2,7 @@
 session_start();
 require_once '../../../php/Chamado.php';
 require_once '../../../php/Email.php';
-require_once '../../../arealateral.php';
+// require_once '../../../arealateral.php';
 if (!isset($_SESSION['usuario_id'])) {
     header("Location: ../../index.php");
     exit();
